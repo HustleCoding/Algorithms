e90fe0082dcec89c3db5102edf526eef9dd73815
HustleCoding <f.dobinciuc7@gmail.com>
2022-10-07 23:57:53 +0300

    Add 704 leetcode solution

---
+++ 704-BinarySearch.py
@@ -0,0 +1,18 @@
+from ast import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left += 1
+            else:
+                right -= 1
+
+        return -1
+            
